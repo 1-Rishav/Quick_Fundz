e39fd3a2880f992a43985d88bba6fea9815612dd
Rishav Raj <rajrishav011@gmail.com>
2024-12-26 14:58:40 +0530

    Add all loan routes

---
+++ server/src/routes/loanRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const loanController = require('../controller/loanController')
+const authenticate = require('../middleware/authenticate.middleware')
+const router = express.Router();
+
+router.post('/loan', loanController.requestLoan);
+router.post("/requestInvestor", loanController.requestInvestor);
+router.post("/rejectLoan", loanController.rejectedLoan);
+router.post("/acceptLoan", loanController.acceptLoan);
+router.post("/lastNegotiate", loanController.lastNegotiate);
+router.post("/paidStatus", loanController.paidStatus);
+router.get("/repayStatus", authenticate,loanController.repayStatus);
+router.post("/moneyRepay", loanController.moneyRepaid);
+
+module.exports = router;
