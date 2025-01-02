251b80b446af976f3f80d0494b99a35ae7de2d23
Rishav Raj <rajrishav011@gmail.com>
2025-01-02 22:12:02 +0530

    change bg & style

--- client/src/components/admin/NegotiationDetails.jsx
+++ client/src/components/admin/NegotiationDetails.jsx
@@ -17,6 +17,7 @@ const statusColorMap = {
 const NegotiationDetails = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [allRequest, setAllRequest] = useState([]);
+  const [countNegotiateData, setCountNegotiateData ] = useState(null);
   const [loading, setLoading] = useState(true);
   const [showInput, setShowInput] = useState(null);
   const [inputValue, setInputValue] = useState(null)
@@ -45,6 +46,7 @@ const NegotiationDetails = () => {
       try {
         const allKycUser = await dispatch(allNegotiateData());
         setAllRequest(allKycUser);
+        setCountNegotiateData(allKycUser.length)
       } catch (error) {
         console.error("Error fetching KYC details:", error);
       } finally {
@@ -146,8 +148,8 @@ const NegotiationDetails = () => {
   return (
     <div className="flex h-screen overflow-hidden bg-gray-100">
       <Sidebar />
-      <div className="flex flex-col flex-1 max-w-8xl mx-auto p-4 overflow-auto">
-        <h1 className="text-2xl font-bold mb-6 text-center md:text-left">KYC Details</h1>
+      <div className="flex flex-col flex-1 max-w-8xl mx-auto p-4 overflow-auto gradient-bg-transactions">
+        <h1 className="text-2xl font-bold mb-6 text-center md:text-left text-neutral-50 flex items-center justify-center">Negotiation Details</h1>
 
         {loading ? (
           <p className="text-center">Loading...</p>
@@ -192,7 +194,7 @@ const NegotiationDetails = () => {
 
 
         {/* Pagination */}
-        <div className="flex justify-end mt-6 space-x-2">
+       {countNegotiateData >7 && <div className="flex justify-end mt-6 space-x-2">
           <button
             className={`px-4 py-2 rounded-full ${currentPage === 1 ? 'bg-gray-300 cursor-not-allowed' : 'bg-slate-800 text-white'}`}
             disabled={currentPage === 1}
@@ -218,7 +220,7 @@ const NegotiationDetails = () => {
           >
             Next
           </button>
-        </div>
+        </div>}
       </div>
     </div>
   );
