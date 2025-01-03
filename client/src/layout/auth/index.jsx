d48eca3e15e7825d92fb9df3af4835cd1de78fdd
Rishav Raj <rajrishav011@gmail.com>
2025-01-03 11:33:06 +0530

    auth for register user

---
+++ client/src/layout/auth/index.jsx
@@ -0,0 +1,36 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const AuthLayout = () => {
+  const { isLoggedIn, role, verificationStatus,verified } = useSelector((state) => state.auth)
+
+
+  if (isLoggedIn) {
+    if (role === "admin"&& verificationStatus==="verified" && verified === true) {
+      return <Navigate to="/admin" />;
+    } else if (role === "user" && verificationStatus==="verified" && verified === true) {
+      return <Navigate to="/" />
+    }
+  }
+
+  /* if (role === "admin" && verificationStatus === "verified") {
+   return <Navigate to="/admin" />;
+ }
+ 
+ if (role === "user") {
+   if (verificationStatus === "verified") {
+     return <Navigate to="/" />;
+   } else {
+     return <Navigate to="/auth/kycstatus" />;
+   }
+ }  */
+
+  return (
+    <>
+      <Outlet />
+
+    </>
+  );
+};
+
+export default AuthLayout;
