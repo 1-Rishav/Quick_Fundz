1adecba950c03e3975aad8374de036dc3261109f
Rishav Raj <rajrishav011@gmail.com>
2025-01-03 11:30:45 +0530

    auth for admin

---
+++ client/src/layout/admin/index.jsx
@@ -0,0 +1,41 @@
+import { Navigate, Outlet } from "react-router-dom";
+
+import { useSelector } from "react-redux";
+
+
+const AdminLayout = () => {
+  const {isLoggedIn,role,verificationStatus,verified} = useSelector((state)=>state.auth)
+  
+
+  
+
+    if (isLoggedIn) {
+    // Ensure that role and verificationStatus exist before navigating
+    if (role === "user" && verificationStatus === "verified" && verified ===true) {
+        return <Navigate to="/" />;
+      }
+    }
+  
+
+   /*    if (role === "user") {
+    if (verificationStatus === "verified") {
+      return <Navigate to="/" />;
+    } else {
+      return <Navigate to="/auth/kycstatus" />;
+    }
+  }  */
+
+     if(!isLoggedIn){
+    return <Navigate to="/auth/home"/>
+  } 
+
+  
+  
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+};
+
+export default AdminLayout;
