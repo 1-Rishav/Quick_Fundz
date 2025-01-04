5b70397d56626b5c5eb8c57f3942608ce2fb208d
Rishav Raj <rajrishav011@gmail.com>
2025-01-04 22:56:24 +0530

    display status to first login user

---
+++ client/src/pages/dashboard/KycStatusPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import KycStatus from '../../components/KycStatus'
+
+function KYCStatus() {
+  return (
+    <>
+    <KycStatus/>
+    </>
+  )
+}
+
+export default KYCStatus
