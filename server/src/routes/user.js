0540435f59ed878d9cfed63bc9c7ed591dcc4610
Rishav Raj <rajrishav011@gmail.com>
2025-01-05 22:12:34 +0530

    add document routes

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const userController = require('../controller/userController')
-const authenticate = require('../middleware/authenticate.middleware')
+const authenticate = require('../middleware/authenticate.middleware');
+const { upload } = require('../middleware/cloudinary.middleware');
 router.post('/login',userController.loginUser)
 router.post('/register',userController.registerUser,userController.sendOTP)
 router.post('/verifyEmail',userController.verifyOTP)
-
+router.post("/incomeDocuments", authenticate ,upload.single('file'),userController.incomeDocuments);
 module.exports = router;
