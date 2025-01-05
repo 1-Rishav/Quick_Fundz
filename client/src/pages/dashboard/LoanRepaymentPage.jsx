dad28cbd614a08cadc03be3a8d9e49372fd6b5e2
Rishav Raj <rajrishav011@gmail.com>
2025-01-05 22:17:01 +0530

    Add repayment page

---
+++ client/src/pages/dashboard/LoanRepaymentPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import RepaymentDetails from '../../components/admin/RepaymentDetails'
+
+function LoanRepaymentPage() {
+  return (
+    <>
+    <RepaymentDetails/>
+    </>
+  )
+}
+
+export default LoanRepaymentPage
