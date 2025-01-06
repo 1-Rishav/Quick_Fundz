56c51e8c4c18d2c2ca9fef570f870a06e03c378d
Rishav Raj <rajrishav011@gmail.com>
2025-01-06 23:38:18 +0530

    negotiation details for admin

---
+++ client/src/pages/dashboard/NegotiationDetailPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NegotiationDetails from '../../components/admin/NegotiationDetails'
+
+function NegotiationDetailPage() {
+  return (
+    <>
+    <NegotiationDetails/>
+    </>
+  )
+}
+
+export default NegotiationDetailPage
