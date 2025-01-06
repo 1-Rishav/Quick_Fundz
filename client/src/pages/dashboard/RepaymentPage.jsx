87ca08e2e3a4886b0bbe275b1b6fe5f72ad94246
Rishav Raj <rajrishav011@gmail.com>
2025-01-06 23:43:46 +0530

    loan repayment page

---
+++ client/src/pages/dashboard/RepaymentPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoanRepayment from '../../components/Repayment'
+
+function RepaymentPage() {
+  return (
+    <>
+    <LoanRepayment/>
+    </>
+  )
+}
+
+export default LoanRepayment
