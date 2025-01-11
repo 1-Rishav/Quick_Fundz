7c927a99fda8f588ac44eef2abd65a9e36611cf5
Rishav Raj <rajrishav011@gmail.com>
2025-01-11 23:23:43 +0530

    Update in home page

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -7,14 +7,23 @@ import { adminMessage, LogoutUser } from '../redux/slices/auth';
 import  frontImg from '../assets/peer-to-peer.jpg'
 import MileStones from './MileStones';
 import ReasonToJoin from './ReasonToJoin';
-import { AppleCardsCarouselDemo } from '../components/Carousel';
 import { BackgroundBeamsWithCollisionDemo } from './Footer_BeamsCollision';
+import UserProfile from './UserProfile';
+import ProfileModal from './ProfileModal';
+import { CarouselDemo } from '../components/Carousel';
 
 const Menu = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [showPopup, setShowPopup] = useState(false);
   const [showMessage, setShowMessage] = useState([]);
+  const [open , setOpen] = useState(false);
+  const [avatarUrl, setAvatarUrl] = useState(""); // State to store avatar URL
+
+  const handleAvatarChange = (newAvatarUrl) => {
+    console.log(newAvatarUrl);
+    setAvatarUrl(newAvatarUrl); // Update avatar URL
+  };
   const popupRef = useRef(null); 
 
   const handleLogout = (e) => {
@@ -22,6 +31,10 @@ const Menu = () => {
     dispatch(LogoutUser());
     navigate("/auth/home");
   };
+  const handleOpen = ()=>{
+    setOpen(prevState=>!prevState)
+}
+
 
   
   const handleNotificationClick = () => {
@@ -50,7 +63,7 @@ const Menu = () => {
 
   return (
     <>
-      <nav className="flex justify-between items-center bg-white p-4 text-black shadow-md">
+      <nav className="flex justify-between items-center bg-gray-300 p-4 text-black shadow-md">
         <h1 className="text-xl font-bold">Quick Fundz</h1>
         <div className="flex items-center">
           <div className="relative inline-block mr-4">
@@ -68,31 +81,47 @@ const Menu = () => {
               </>
             )}
           </div>
+          <UserProfile avatarUrl={avatarUrl} handleOpen={handleOpen}/>
+          
+        </div>
+      </nav>
+      {open && (
+        <div className='h-52 w-full flex items-end justify-center  mt-1 flex-col absolute z-50'>
+          <div className='h-52 w-[16rem] bg-gray-50 flex flex-col justify-start items-start rounded-xl mr-5 '>
+          <ProfileModal onAvatarChange={handleAvatarChange}/>
           <button 
             type='submit' 
             onClick={handleLogout} 
-            className="ml-4 bg-white px-4 py-2 rounded-xl hover:bg-gray-200"
+            className=" flex w-full  bg-gray-300 px-4 py-2 mt-2 hover:bg-gray-200"
+          >
+            Your Investment
+          </button>
+          <button 
+            type='submit' 
+            onClick={handleLogout} 
+            className=" flex w-full  bg-gray-300 px-4 py-2 mt-2 hover:bg-gray-200"
           >
             Logout
           </button>
+          </div>
         </div>
-      </nav>
+      )} 
       <div className="flex h-screen w-full  overflow-hidden">
         <div className="z-index-50 absolute">
           <Sidebar />
         </div>
         <div className="relative h-screen w-screen   gradient-bg-welcome  ">
-          <div className='flex flex-wrap justify-between items-center h-[85vh] w-screen object-contain'>
-          <div className="relative left-28 flex flex-col items-start justify-center h-full text-neutral-100">
+          <div className='flex flex-wrap justify-between items-center h-[85vh] w-full object-contain'>
+          <div className="relative ml-28 flex flex-col items-start justify-center h-fit w-fit text-neutral-100">
             <h1 className="text-4xl font-bold ">Your Financial Needs, Our Trusted Solution.</h1>
             <p className="text-xl mt-4">Simplifying borrowing and lending, one connection at a time.Bringing lenders and <br/> borrowers together for mutual growth.</p>
           </div>
-          <div className='relative  flex justify-center items-center top-16  w-[38%] h-[50%] '>
-            <img src={frontImg} alt="Peer to peer img" className='rounded-xl h-[100%]'/>
+          <div className='relative  flex flex-wrap justify-center items-center xl:top-16 ml-24 md:w-full 2xl:w-[38%] sm:h-[50%] h-[30%] '>
+            <img src={frontImg} alt="Peer to peer img" className='rounded-xl h-[100%] sm:h-[100%] 2xl:w-[90%] '/>
           </div>
           </div>
-          <div className='flex flex-wrap items-center  justify-center gap-3'>
-          <div className='p-2 white-glassmorphism flex items-center justify-center gap-2 text-neutral-100 text-2xl  h-fit w-fit'>
+          <div className=' max-md:hidden flex flex-wrap items-center  justify-center gap-3'>
+          <div className=' p-2 white-glassmorphism flex items-center justify-center gap-2 text-neutral-100 text-2xl  h-fit w-fit'>
             <h3>Secure Digitised Processes | </h3>
             <h3>Transforming Financial Opportunities | </h3>
             <h3>Earn More with Your Savings</h3>
@@ -104,7 +133,7 @@ const Menu = () => {
       <div className='pb-28 relative h-full w-full gradient-bg-transactions text-neutral-50'>
         <MileStones/>
           <ReasonToJoin/>
-         <AppleCardsCarouselDemo/>
+         <CarouselDemo/>
          <BackgroundBeamsWithCollisionDemo/>
         </div>
     </>
