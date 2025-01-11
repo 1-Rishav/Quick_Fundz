17b39e2a9b06fc126973231d281739a32928ef04
Rishav Raj <rajrishav011@gmail.com>
2025-01-11 23:20:52 +0530

    show current data of app

---
+++ client/src/components/ReasonToJoin.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import joinplatform from '../assets/WhytoJoin.jpg'
+import GraphCharts from './GraphCharts'
+
+function ReasonToJoin() {
+  return (
+
+    <div className='relative top-16 text-3xl flex flex-col items-center justify-center '>
+        <div>
+        <h1 className='text-5xl font-bold '>Our Platform Performance Insights</h1>
+        </div>
+        {/* <div className='relative top-5 h-[58vh] w-full'>
+            <img src={joinplatform} alt="Why P2P" className='rounded-sm h-full w-full'/>
+        </div> */}
+       <div className='flex items-center justify-center rounded-xl gradient-bg-services h-full w-full object-contain mt-8'>
+        <GraphCharts/>
+       </div>
+    </div>
+
+  )
+}
+
+export default ReasonToJoin
