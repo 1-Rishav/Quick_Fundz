1ab62c60bc865f63e51c570467603200ffcf756d
Rishav Raj <rajrishav011@gmail.com>
2025-01-12 22:49:11 +0530

    carousel data

---
+++ client/src/components/CarouselData.js
@@ -0,0 +1,25 @@
+import WhatP2P from '../assets/WhatP2P.jpg'
+import WhytoJoin from '../assets/WhytoJoin.jpg'
+import advantage from '../assets/Advantage.jpg'
+import peer2peer from '../assets/peer-to-peer.jpg'
+
+export const data =[
+
+    {
+        id:1,
+        src:WhatP2P,
+    },
+    {
+        id:2,
+        src:WhytoJoin,
+    },
+    {
+        id:3,
+        src:peer2peer,
+    },
+    {
+        id:4,
+        src:advantage,
+    },
+
+]
