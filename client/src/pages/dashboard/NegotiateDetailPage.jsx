0913fd23cc04e25ae0a4715d4e13a1610883f0b9
Rishav Raj <rajrishav011@gmail.com>
2025-01-22 23:29:58 +0530

    add negotiatedetail for small screen

---
+++ client/src/pages/dashboard/NegotiateDetailPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import NegotiateDetail from '../../components/NegotiateDetail'
+
+function NegotiateDetailPage() {
+  return (
+    <NegotiateDetail/>
+  )
+}
+
+export default NegotiateDetailPage
