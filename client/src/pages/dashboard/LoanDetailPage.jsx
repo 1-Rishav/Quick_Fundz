566cf0c83b368b067a76fe26f5442ef44751670d
Rishav Raj <rajrishav011@gmail.com>
2025-01-22 23:29:22 +0530

    add loandetail page small screen

---
+++ client/src/pages/dashboard/LoanDetailPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoanDetail from '../../components/LoanDetail'
+
+function LoanDetailPage() {
+  return (
+<>
+<LoanDetail/>
+</>
+)
+}
+
+export default LoanDetailPage
