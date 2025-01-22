cdc9f2ed21b930a6cfb11dc129dddc4a047d9dce
Rishav Raj <rajrishav011@gmail.com>
2025-01-22 23:30:33 +0530

    repaymentdetail for small screen

---
+++ client/src/pages/dashboard/RepaymentDetailPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import RepaymentDetail from '../../components/RepaymentDetail'
+
+function RepaymentDetailPage() {
+  return (
+    <>
+    <RepaymentDetail/>
+    </>
+  )
+}
+
+export default RepaymentDetailPage
