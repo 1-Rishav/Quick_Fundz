52f7d962dce2249df2ce26560ccb24bbb4400a9b
Rishav Raj <rajrishav011@gmail.com>
2025-01-28 18:43:45 +0530

    add new pages

---
+++ client/src/pages/dashboard/Investor_InvestmentPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Investor_Investment from '../../components/Investor_Investment'
+
+const Investor_InvestmentPage = () => {
+  return (
+    <Investor_Investment/>
+  )
+}
+
+export default Investor_InvestmentPage
