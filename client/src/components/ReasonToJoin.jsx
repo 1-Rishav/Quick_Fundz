42b2a2d021e005fc0a6604213aec506b3e7d65c0
Rishav Raj <rajrishav011@gmail.com>
2025-01-31 23:53:33 +0530

    update file

--- client/src/components/ReasonToJoin.jsx
+++ client/src/components/ReasonToJoin.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import joinplatform from '../assets/WhytoJoin.jpg'
 import GraphCharts from './GraphCharts'
 
-function ReasonToJoin() {
+function ReasonToJoin({handleDetail}) {
   return (
 
     <div className='relative top-16 text-3xl flex flex-col items-center justify-center '>
@@ -13,7 +13,7 @@ function ReasonToJoin() {
             <img src={joinplatform} alt="Why P2P" className='rounded-sm h-full w-full'/>
         </div> */}
        <div className='flex items-center justify-center rounded-xl gradient-bg-services h-full w-full object-contain mt-8'>
-        <GraphCharts/>
+        <GraphCharts platformDetail={handleDetail}/>
        </div>
     </div>
 
