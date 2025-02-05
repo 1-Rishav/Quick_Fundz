0460b13f63867851ad340b98cdd418897ba78cca
Rishav Raj <rajrishav011@gmail.com>
2025-02-05 17:40:32 +0530

    write success error message

--- server/src/controller/loancontroller.js
+++ server/src/controller/loancontroller.js
@@ -10,68 +10,134 @@ let pool;
 
 exports.acceptedLoan = asyncHandler(async (req, res) => {
     const { investorId,investorUserId,investorAmount,investorDuration,investorRate,investorEmail,user_id,status } = req.body;
-    const filteredBody = filterObj(req.body, 'investorId', 'investorUserId', 'investorAmount', 'investorDuration', 'investorRate', 'user_id', 'status','investorEmail');
-    const user = await pool.query("Select * from users where id=$1", [user_id])
-    const name = user.rows[0].name;
-    const email = user.rows[0].email;
-    const investorStatus = await pool.query("UPDATE investor_details SET invest_status = $1 WHERE id = $2", [status, investorId]);
-    const loanRequest = await pool.query(
-        "INSERT INTO loan_request_details (name, loan_amount, duration, user_id, rate_of_interest, email, investor_email, investoruser_id, original_amount, original_duration, original_rate_of_interest,investor_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11,$12)",
-        [name, filteredBody.investorAmount, filteredBody.investorDuration, filteredBody.user_id, filteredBody.investorRate, email, filteredBody.investorEmail, filteredBody.investorUserId, filteredBody.investorAmount, filteredBody.investorDuration, filteredBody.investorRate, investorId]
+    try {
+        const filteredBody = filterObj(req.body, 'investorId', 'investorUserId', 'investorAmount', 'investorDuration', 'investorRate', 'user_id', 'status','investorEmail');
+        const user = await pool.query("Select * from users where id=$1", [user_id])
+        const name = user.rows[0].name;
+        const email = user.rows[0].email;
+        const investorStatus = await pool.query("UPDATE investor_details SET invest_status = $1 WHERE id = $2", [status, investorId]);
+        const loanRequest = await pool.query(
+            "INSERT INTO loan_request_details (name, loan_amount, duration, user_id, rate_of_interest, email, investor_email, investoruser_id, original_amount, original_duration, original_rate_of_interest,investor_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11,$12)",
+            [name, filteredBody.investorAmount, filteredBody.investorDuration, filteredBody.user_id, filteredBody.investorRate, email, filteredBody.investorEmail, filteredBody.investorUserId, filteredBody.investorAmount, filteredBody.investorDuration, filteredBody.investorRate, investorId]
+    
+        );
+        const updateLoanUser_id = await pool.query("Update users set loan_usersuser_id=$1 where id=$2", [user_id, user_id])
 
-    );
-    const updateLoanUser_id = await pool.query("Update users set loan_usersuser_id=$1 where id=$2", [user_id, user_id])
+        return res.status(200).json({
+            message:"Loan accepted"
+        })
+    } catch (error) {
+        return res.status(500).json({message:"Error occured while processing"});
+    }
 
 })
 exports.requestLoan = asyncHandler(async (req, res) => {
     const { userId, loanAmount, loanDuration, loanUserId, loanInterestRate, investorEmail, investorUserId, status, investorAmount, investorDuration, investorRate } = req.body;
-    const filteredBody = filterObj(req.body, 'loanAmount', 'loanDuration', 'loanUserId', 'loanInterestRate', 'investorEmail', 'investorUserId', 'status', 'investorAmount', 'investorDuration', 'investorRate');
-    const user = await pool.query("Select * from users where id=$1", [loanUserId])
-    const name = user.rows[0].name;
-    const email = user.rows[0].email;
-    const investorStatus = await pool.query("UPDATE investor_details SET invest_status = $1 WHERE id = $2", [status, userId]);
-    const loanRequest = await pool.query(
-        "INSERT INTO loan_request_details (name, loan_amount, duration, user_id, rate_of_interest, email, investor_email, investoruser_id, original_amount, original_duration, original_rate_of_interest,investor_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11,$12)",
-        [name, filteredBody.loanAmount, filteredBody.loanDuration, filteredBody.loanUserId, filteredBody.loanInterestRate, email, filteredBody.investorEmail, filteredBody.investorUserId, filteredBody.investorAmount, filteredBody.investorDuration, filteredBody.investorRate, userId]
-
-    );
-    const updateLoanUser_id = await pool.query("Update users set loan_usersuser_id=$1 where id=$2", [loanUserId, loanUserId])
+    try {
+        const filteredBody = filterObj(req.body, 'loanAmount', 'loanDuration', 'loanUserId', 'loanInterestRate', 'investorEmail', 'investorUserId', 'status', 'investorAmount', 'investorDuration', 'investorRate');
+        const user = await pool.query("Select * from users where id=$1", [loanUserId])
+        const name = user.rows[0].name;
+        const email = user.rows[0].email;
+        const investorStatus = await pool.query("UPDATE investor_details SET invest_status = $1 WHERE id = $2", [status, userId]);
+        const loanRequest = await pool.query(
+            "INSERT INTO loan_request_details (name, loan_amount, duration, user_id, rate_of_interest, email, investor_email, investoruser_id, original_amount, original_duration, original_rate_of_interest,investor_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11,$12)",
+            [name, filteredBody.loanAmount, filteredBody.loanDuration, filteredBody.loanUserId, filteredBody.loanInterestRate, email, filteredBody.investorEmail, filteredBody.investorUserId, filteredBody.investorAmount, filteredBody.investorDuration, filteredBody.investorRate, userId]
+    
+        );
+        const updateLoanUser_id = await pool.query("Update users set loan_usersuser_id=$1 where id=$2", [loanUserId, loanUserId])
+        return res.status(200).json({
+            message:"Request under processing"
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message:"Error processing request"
+        })
+    }
 
 })
 
 exports.requestInvestor = asyncHandler(async (req, res) => {
     const { investorUserId } = req.body;
 
-    const userLoanRequest = await pool.query("SELECT * from loan_request_details where investoruser_id=$1 AND state!=$2 ", [investorUserId, 'Rejected'])
-
-    const loanTaker = userLoanRequest.rows
-    //const loan_status=userLoanRequest.rows[0].state;
-
-    res.status(200).json({
-        loanTaker
-    })
+    try {
+        const userLoanRequest = await pool.query("SELECT * from loan_request_details where investoruser_id=$1 AND state!=$2 ", [investorUserId, 'Rejected'])
+    
+        const loanTaker = userLoanRequest.rows
+        //const loan_status=userLoanRequest.rows[0].state;
+    
+       return res.status(200).json({
+        message:"Fetched requested loans",
+            loanTaker
+        })
+    } catch (error) {
+        return res.status(401).json({
+            message:"Error occured"
+        })
+    }
+})
+exports.smallRequest = asyncHandler(async (req, res)=>{
+    const {id,user_id} = req.body;
+    try {
+        const userLoanRequest = await pool.query("SELECT * from loan_request_details where id=$1 AND state!=$2 AND investoruser_id=$3", [id, 'Rejected',user_id ])
+    
+        const requestLoan = userLoanRequest.rows
+        //const loan_status=userLoanRequest.rows[0].state;
+    
+       return res.status(200).json({
+        message:"Fetched requested loans",
+            requestLoan
+        })
+    } catch (error) {
+        message:"Error occured"
+    }
 })
 
 exports.rejectedLoan = asyncHandler(async (req, res) => {
     const { status, userId, loanId } = req.body;
-    const updateLoanRequest = await pool.query("UPDATE loan_request_details SET state=$1,pay_status=$2 where id=$3 ", [status, status, loanId])
-    const updateInvestor = await pool.query("Update investor_details Set invest_status=$1 where id=$2", ['provider', userId])
+    try {
+        const updateLoanRequest = await pool.query("UPDATE loan_request_details SET state=$1,pay_status=$2 where id=$3 ", [status, status, loanId])
+        const updateInvestor = await pool.query("Update investor_details Set invest_status=$1 where id=$2", ['provider', userId])
+        return res.status(200).json({
+            message:"Loan rejected successfully"
+        })
+    } catch (error) {
+        message:"Error occured while processing"
+    }
 
 })
 
 exports.acceptLoan = asyncHandler(async (req, res) => {
     const { status, userId, loanId, pay_status } = req.body;
-    const updateLoanRequest = await pool.query("UPDATE loan_request_details SET state=$1,pay_status=$2 where id=$3 ", [status, pay_status, loanId])
+    try {
+        const updateLoanRequest = await pool.query("UPDATE loan_request_details SET state=$1,pay_status=$2 where id=$3 ", [status, pay_status, loanId])
+        return res.status(200).json({
+            message:"Loan accepted successfully"
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message:"Error occured while processing"
+        })
+    }
     //const updateInvestor = await pool.query("Update investor_details Set invest_status=$1 where id=$2", ['paid', userId])
 });
 
 exports.lastNegotiate = asyncHandler(async (req, res) => {
     const { investor_email, investoruser_id, negotiateAmount, negotiateDuration, negotiateInterestRate, loan_amount, loan_duration, loanInterestRate, loanUserId, loanId } = req.body;
-    const filteredBody = filterObj(req.body, 'investor_email', 'investoruser_id', 'negotiateAmount', 'negotiateDuration', 'negotiateInterestRate', 'loan_amount', 'loan_duration', 'loanInterestRate', 'loanUserId', 'loanId')
-    const user = await pool.query("SELECT * FROM users where id=$1", [investoruser_id])
-    const name = user.rows[0].name;
-    const insertNegotiateRequest = await pool.query("INSERT INTO investor_negotiation (name,email,user_id,negotiate_amount,negotiate_duration,negotiate_rate_of_interest,loan_amount,loan_duration,loan_rate_of_interest,loan_user_id,loan_id) Values($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)", [name, filteredBody.investor_email, filteredBody.investoruser_id, filteredBody.negotiateAmount, filteredBody.negotiateDuration, filteredBody.negotiateInterestRate, filteredBody.loan_amount, filteredBody.loan_duration, filteredBody.loanInterestRate, filteredBody.loanUserId, filteredBody.loanId])
-    const updateLoanRequstDetails = await pool.query("Update loan_request_details set original_amount=$1 , original_duration=$2 ,original_rate_of_interest=$3 where investoruser_id=$4", [filteredBody.negotiateAmount, filteredBody.negotiateDuration, filteredBody.negotiateInterestRate, filteredBody.investoruser_id])
+
+    try {
+        const filteredBody = filterObj(req.body, 'investor_email', 'investoruser_id', 'negotiateAmount', 'negotiateDuration', 'negotiateInterestRate', 'loan_amount', 'loan_duration', 'loanInterestRate', 'loanUserId', 'loanId')
+        const user = await pool.query("SELECT * FROM users where id=$1", [investoruser_id])
+        const name = user.rows[0].name;
+        const insertNegotiateRequest = await pool.query("INSERT INTO investor_negotiation (name,email,user_id,negotiate_amount,negotiate_duration,negotiate_rate_of_interest,loan_amount,loan_duration,loan_rate_of_interest,loan_user_id,loan_id) Values($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)", [name, filteredBody.investor_email, filteredBody.investoruser_id, filteredBody.negotiateAmount, filteredBody.negotiateDuration, filteredBody.negotiateInterestRate, filteredBody.loan_amount, filteredBody.loan_duration, filteredBody.loanInterestRate, filteredBody.loanUserId, filteredBody.loanId])
+        const updateLoanRequstDetails = await pool.query("Update loan_request_details set original_amount=$1 , original_duration=$2 ,original_rate_of_interest=$3 where investoruser_id=$4", [filteredBody.negotiateAmount, filteredBody.negotiateDuration, filteredBody.negotiateInterestRate, filteredBody.investoruser_id])
+        return res.status(200).json({
+            message:"Negotiation Request sent successfully"
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message:"Error sending request"
+        })
+    }
 })
 
 exports.paidStatus = asyncHandler(async (req, res) => {
@@ -97,7 +163,8 @@ exports.paidStatus = asyncHandler(async (req, res) => {
         let interest = (Amount * Anual_duration * ROI) / 100;
         let total_amount = Math.ceil(Amount + interest);
         let remainDays = endDate - startDate;
-        const days = remainDays / (1000 * 60 * 60 * 24);
+        const days = +(remainDays / (1000 * 60 * 60 * 24));
+
         const insertLoanRepaymentData = await pool.query('Insert into loan_repayment (name,loan_amount,loan_duration,loan_roi,interest_amount,remain_days,repayment_user_id,provider_user_id) values ($1,$2, $3,$4,$5,$6,$7,$8)', [investorName, loan_amount, loan_duration, loan_ROI, total_amount, days, repay_user, provider_user])
         const updateUserId = await pool.query('Update users set repayment_usersuser_id = $1 where id=$2', [repay_user, repay_user])
 
@@ -106,8 +173,9 @@ exports.paidStatus = asyncHandler(async (req, res) => {
             message: 'Payment Successful'
         })
     } catch (error) {
-        console.log(error);
-    }
+return res.status(500).json({
+    message:"Payment Failed"
+})    }
 })
 
 exports.repayStatus = asyncHandler(async (req, res) => {
@@ -115,9 +183,24 @@ exports.repayStatus = asyncHandler(async (req, res) => {
     try {
         const RepayData = await pool.query('Select * from loan_repayment where repayment_user_id = $1', [userId])
         const repayData = RepayData.rows
-        res.status(200).json(repayData)
+        res.status(200).json({message:"Fetched all Repayment data",repayData})
     } catch (error) {
-        console.log(error);
+        return res.status(500).json({
+            message:"Error fetching Repayment data"
+        })
+    }
+})
+
+exports.smallRepayment = asyncHandler(async (req, res) => {
+    const { id , user_id} = req.body;
+    try {
+        const RepayData = await pool.query('Select * from loan_repayment where id=$1 AND repayment_user_id = $2', [id, user_id])
+        const repayData = RepayData.rows
+       return res.status(200).json({message:"Fetched all Repayment data",repayData})
+    } catch (error) {
+        return res.status(500).json({
+            message:"Error fetching Repayment data"
+        })
     }
 })
 
@@ -125,9 +208,14 @@ exports.moneyRepaid = asyncHandler(async (req, res) => {
     const { pay_id, paid_status } = req.body;
     try {
         const updateRepayment = await pool.query('Update loan_repayment set payment_status = $1 paid_time = $2 where id=$3', [paid_status, new Date(Date.now()), pay_id])
+
+        return res.status(200).json({
+            message:"Repayment Successfull"
+        })
     } catch (error) {
-        console.log(error);
-    }
+return res.status(500).json({
+    message:"Repayment Failed"
+})    }
 })
 
 exports.borrowRequest = asyncHandler(async (req, res) => {
@@ -141,9 +229,13 @@ exports.borrowRequest = asyncHandler(async (req, res) => {
         const fetchName = await pool.query('Select name from users where id=$1',[userId])
         const name = await fetchName.rows[0].name;
         const insertpreLoanRequest = await pool.query('Insert into loanpre_request  (name,email ,amount , duration , rate_of_interest,user_id) values($1,$2,$3,$4,$5,$6)',[name,email,amount,duration,interestRate,userId])
+        return res.status(200).json({
+            message:"We will notify once requirements matched"
+        })
     } catch (error) {
-        console.log(error)
-    }
+return res.status(500).json({
+    message:"Error submitting request"
+})    }
 })
 
 corn.schedule('0 0 * * *', async () => {
