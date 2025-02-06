c190958eb9ffa5e571418cc1ed9d7c0ff7c005ef
Rishav Raj <rajrishav011@gmail.com>
2025-02-06 18:02:17 +0530

    update styles

--- client/src/components/LoanPre_Request.jsx
+++ client/src/components/LoanPre_Request.jsx
@@ -8,7 +8,6 @@ function LoanPre_Request() {
   const [duration, setDuration] = useState("");
   const [interestRate, setInterestRate] = useState("");
   const [loading, setLoading] = useState(false);
-  const [message, setMessage] = useState("");
   const [error, setError] = useState("");
 
   const dispatch = useDispatch();
@@ -17,18 +16,16 @@ function LoanPre_Request() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
-    setMessage("");
     try {
       const data = { email, amount, duration, interestRate,userId };
       dispatch(loanPreRequest(data));
       
-      setMessage("Loan Request submitted successfully.");
       setEmail("");
       setAmount("");
       setDuration("");
       setInterestRate("");
     } catch (error) {
-      setMessage("Error sending request data.");
+      console.log(error);
     } finally {
       setLoading(false);
     }
@@ -49,10 +46,10 @@ function LoanPre_Request() {
 
   return (
     <div className="flex-1 flex-col flex items-center justify-center min-h-screen p-4 gradient-bg-transactions text-neutral-50">
-        <h1 className=" w-full text-white flex items-center justify-center text-4xl font-bold mb-6 text-center">Submit Your Request</h1>
+        <h1 className=" w-full p-6  text-white flex items-center justify-center text-lg md:text-2xl lg:text-3xl xl:text-4xl  font-bold  text-center">Submit Your Request</h1>
     <div className="gradient-bg-services text-neutral-50 p-8 rounded-2xl shadow-lg w-full max-w-lg">
       <h1 className="text-3xl font-bold  mb-6 text-center">
-        Investment Form
+        Loan-Request
       </h1>
       <form onSubmit={handleSubmit}>
         <div className="mb-4">
@@ -64,7 +61,7 @@ function LoanPre_Request() {
             placeholder="Enter email"
             value={email}
             onChange={handleChangeEmail}
-            className="p-3 border text-black border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-blue-400"
+            className="p-2 border text-black border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-blue-400"
             required
           />
         </div>
@@ -77,7 +74,7 @@ function LoanPre_Request() {
             placeholder="Enter amount"
             value={amount}
             onChange={handleChangeAmount}
-            className="p-3 border text-black border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-blue-400"
+            className="p-2 border text-black border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-blue-400"
             required
           />
         </div>
@@ -90,7 +87,7 @@ function LoanPre_Request() {
             placeholder="Enter duration (e.g., 2 months)"
             value={duration}
             onChange={handleChangeDuration}
-            className="p-3 border text-black border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-blue-400"
+            className="p-2 border text-black border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-blue-400"
             required
           />
         </div>
@@ -103,7 +100,7 @@ function LoanPre_Request() {
             placeholder="Enter interest rate (e.g., 5%)"
             value={interestRate}
             onChange={handleChangeROI}
-            className="p-3 border text-black border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-blue-400"
+            className="p-2 border text-black border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-blue-400"
             required
           />
         </div>
@@ -113,15 +110,7 @@ function LoanPre_Request() {
         >
           {loading ? "Submitting..." : "Submit"}
         </button>
-        {message && (
-          <div
-            className={`text-center mt-4 ${
-              message.startsWith("Error") ? "text-red-500" : "text-green-500"
-            }`}
-          >
-            {message}
-          </div>
-        )}
+        
       </form>
     </div>
   </div>
